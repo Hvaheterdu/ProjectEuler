ca67ac83d764b795cd932e2a8991b1a4d81571ab
Burhan Sarfraz <burhans941@gmail.com>
2020-10-13 01:50:33 +0200

    Solved task 5

---
+++ Solutions/p005.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import numpy as np
+
+"""
+Problem name:
+
+Smallest multiple
+
+Problem description:
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+
+def smallest_multiple():
+    total = 1
+    for i in range(1, 21, 1):
+        total *= i // np.gcd(i, total)
+    return total
+
+
+if __name__ == "__main__":
+    print(smallest_multiple())
