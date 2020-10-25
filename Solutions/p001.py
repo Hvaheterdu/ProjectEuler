89ccf979834ae6b5f4212758abd688850eb0e826
Burhan Sarfraz <burhansarfraz@burhans-mbp.lan>
2020-10-26 00:17:08 +0100

    Changed to python list comprehension

--- Solutions/p001.py
+++ Solutions/p001.py
@@ -20,11 +20,7 @@ def multiples():
     Returns:
         int: sum of all multiples
     """
-    res = 0
-    for i in range(1000):
-        if i % 3 == 0 or i % 5 == 0:
-            res += i
-    return res
+    return (i for i in range(1000) if i % 3 == 0 or i % 5 == 0)
 
 
 if __name__ == "__main__":
