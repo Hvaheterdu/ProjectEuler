f0528e708440b68824710acd4aa2b88dcea8b198
Burhan Sarfraz <burhans941@gmail.com>
2020-11-08 21:58:49 +0100

    Solution to problem 12

--- Solutions/p012.py
+++ Solutions/p012.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import math
+import itertools
+import sys
 
 """
 Problem name:
@@ -24,3 +27,45 @@ We can see that 28 is the first triangle number to have over five divisors.
 
 What is the value of the first triangle number to have over five hundred divisors?
 """
+
+N = sys.maxsize
+
+
+def triangle_number(n, div_num):
+    """Computes smallest triangle number with
+    div_num divisors
+
+    Args:
+        n (int): maximum int size
+        div_num (int): number of divisors
+
+    Returns:
+        int: triangle number with div_num divisors
+    """
+    tri_nums = itertools.accumulate(range(1, n))
+    for i in tri_nums:
+        if divisor_function(i) > div_num:
+            return i
+
+
+def divisor_function(n):
+    """Divisor function
+
+    Args:
+        n (int): maximum int size
+
+    Returns:
+        int: number of divisors for a given number
+    """
+    c = 2
+    end = math.sqrt(n)
+    for i in range(1, int(end)):
+        if n % i == 0:
+            c += 2
+    if int(end) == n:
+        c += 1
+    return c
+
+
+if __name__ == "__main__":
+    print(triangle_number(N, 500))
