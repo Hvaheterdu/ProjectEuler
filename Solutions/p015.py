2785fcf5658552478db496bb254b17ca06cf259d
Burhan Sarfraz <burhansarfraz@burhans-mbp.lan>
2020-11-11 21:58:38 +0100

    Solution to problem 14

---
+++ Solutions/p015.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import math
+
+"""
+Problem name: Lattice paths
+
+Problem ID:   15
+"""
+
+
+def lattice_paths(n, k):
+    """Find how many lattice paths there are from
+    (0, 0) to (n, k). Using formula from Wikipedia.
+    See https://en.wikipedia.org/wiki/Lattice_path
+
+    Args:
+        n (int): x-cord for end
+        k (int): y-cord for end
+
+    Returns:
+        int: number of lattice paths
+    """
+    # This is the binomial coefficient (n + k)
+    #                                  (  n  )
+    return math.comb(n, k)
+
+
+if __name__ == "__main__":
+    print(lattice_paths(40, 20))
