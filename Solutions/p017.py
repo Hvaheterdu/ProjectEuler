85a1dd25ec7244053edf29ef3ef2962faa9f69f4
Burhan Sarfraz <burhansarfraz@burhans-mbp.lan>
2020-11-14 02:29:37 +0100

    Solution to problem 17

---
+++ Solutions/p017.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from num2words import num2words
+
+
+"""
+Problem name: Number letter counts
+
+Problem ID:   17
+"""
+
+
+IGNORED_CHAR = [' ', '-']
+
+
+def number_letter_counts(n) -> int:
+    """Calculate sum of all letters when we convert
+    all numbers from 1 to n written in words """
+    res = 0
+    for i in range(1, n + 1):
+        res += len(num2words(i)) - num2words(i).count(' ') - \
+            num2words(i).count('-')
+    return res
+
+
+if __name__ == "__main__":
+    print(number_letter_counts(1000))
