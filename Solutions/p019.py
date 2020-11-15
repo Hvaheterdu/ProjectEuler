106c8307b0db9d0504d5e08fbf210df2b8970e6b
Burhan Sarfraz <burhansarfraz@burhans-mbp.lan>
2020-11-15 04:00:17 +0100

    Solution to problem 19

---
+++ Solutions/p019.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+import datetime
+from datetime import date
+
+
+"""
+Problem name: Counting sundays
+
+Problem ID:   19
+"""
+
+
+def counting_sundays():
+    """Return sum of all sundays that are the first 
+    day in the month from 1901 to (and inclusive) 2000-09-30"""
+    res = 0
+    for i in range(1901, 2001):
+        for j in range(1, 13):
+            res += (1 if datetime.date(i, j, 1).weekday() == 6 else 0)
+    return res
+
+
+if __name__ == "__main__":
+    print(counting_sundays())
